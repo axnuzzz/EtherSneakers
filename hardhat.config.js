ded381c85a68e41176c772bac0929133fc4a5378
axnuzzz <98628805+axnuzzz@users.noreply.github.com>
2023-02-19 21:45:11 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,9 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  defaultNetwork: "hyperspace",
   networks: {
-    hyperspace: {
-      chainId: 3141,
-      url: "https://api.hyperspace.node.glif.io/rpc/v1",
+    mumbai: {
+      url: process.env.URL,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
